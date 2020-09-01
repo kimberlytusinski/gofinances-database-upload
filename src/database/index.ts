7f1ea3d7dd46288518369e081a5a39f9f7b1f84f
KimberlyTusinski <kimberly.tusinski@hotmail.com>
2020-09-01 01:21:38 -0300

    :ok_hand: challenge finished

--- src/database/index.ts
+++ src/database/index.ts
@@ -8,7 +8,7 @@ export default async (name = 'default'): Promise<Connection> => {
       name,
       database:
         process.env.NODE_ENV === 'test'
-          ? 'gostack_desafio06_tests'
+          ? 'transactions_control_tests'
           : defaultOptions.database,
     }),
   );
